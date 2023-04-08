214fc864af8d200455e8770c4535bef89ef9f691
Enzo CECILLON <enzocecillon@macbook-pro-de-enzo.home>
2023-04-08 10:50:20 +0200

    refactor: export script

--- export_to_vm.sh
+++ export_to_vm.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-rsync -rauL /Users/enzocecillon/Documents/FAC/mif-08-2023-cecillon/TP* /Users/enzocecillon/Documents/FAC/mif-08-2023-cecillon/MiniC p1805901@192.168.153.167:/home/p1805901
+rsync -rauL /Users/enzocecillon/Documents/FAC/mif-08-2023-cecillon/TP* \
+  /Users/enzocecillon/Documents/FAC/mif-08-2023-cecillon/MiniC \
+  p1805901@192.168.153.167:/home/p1805901
